312d916eeeb7499363485a60b755621ff79f25e0
qinlanggen <513354668@qq.com>
2024-10-28 09:23:34 +0800

    first init

---
+++ utils/logs/logs.go
@@ -0,0 +1,19 @@
+package logs
+
+import "github.com/sirupsen/logrus"
+
+func Debug(fields map[string]interface{}, msg string) {
+	logrus.WithFields(fields).Debug(msg)
+}
+
+func Info(fields map[string]interface{}, msg string) {
+	logrus.WithFields(fields).Info(msg)
+}
+
+func Warning(fields map[string]interface{}, msg string) {
+	logrus.WithFields(fields).Warning(msg)
+}
+
+func Error(fields map[string]interface{}, msg string) {
+	logrus.WithFields(fields).Error(msg)
+}
